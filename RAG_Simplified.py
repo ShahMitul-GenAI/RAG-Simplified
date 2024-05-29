0033116613eb20701e64a545292340252a93eea9
ShahMitul-GenAI <replymitul@outlook.com>
2024-05-28 23:29:28 -0400

    renamed RAG_Simplified file, refactored that file to be modular

--- RAG_Simplified.py
+++ RAG_Simplified.py
@@ -1,5 +1,5 @@
-
 import time
+import json
 import os.path
 import pickle
 import streamlit as st
@@ -15,171 +15,93 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.utilities import WikipediaAPIWrapper 
 from langchain.text_splitter import RecursiveCharacterTextSplitter 
 from langchain_community.vectorstores import DocArrayInMemorySearch
+from typing import Any, Literal, get_args
 
+DataSource = Literal["Wikipedia", "Research Paper"]
+SUPPORTED_DATA_SOURCES = get_args(DataSource)
 
 # loading API keys from env
 load_dotenv()
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-
-
 # loading model and defining embedding
 llm = ChatOpenAI(temperature=0, model='gpt-3.5-turbo')
 embeddings = OpenAIEmbeddings()
 
+def get_query(source: DataSource) -> str:
+    if source not in SUPPORTED_DATA_SOURCES:
+        raise ValueError(f"Provided data source {source} is not supported.")
+
+    if source == "Wikipedia":
+        filename = "wiki_query.txt"
+    else:
+        filename = "doc_query.txt"
+
+    assert os.path.exists(f"./notifications/{filename}")
+
+    with open(f"./notifications/{filename}", 'r') as f:
+        query = f.read()
+    
+    return query
+
+def load_data_set(source: DataSource, query: str):
+    if source not in SUPPORTED_DATA_SOURCES:
+        raise ValueError(f"Provided data source {source} is not supported.")
+
+    if source == "Wikipedia":
+        Wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+        data = Wikipedia.run(query)
+    else:
+        loader = PyPDFLoader("./2312.10997v5.pdf")
+        data = loader.load()
+
+    # fragmegting the document content to fit in the number of token limitations
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 50)
+
+    if source == "Wikipedia":
+        split_docs = [Document(page_content=sent) for sent in data.split('\n')]
+    else:
+        split_docs = text_splitter.split_documents(data)
+
+    data_set = DocArrayInMemorySearch.from_documents(documents = split_docs, embedding = embeddings)
+    print(type(data_set))
+
+    return data_set
 
 
-# importing inputs from UI 
-fslk = open("./notifications/option.txt", "r")
-selection = fslk.read()
-fslk.close()
+def retrieve_info(source: DataSource, data_set: Any, query: str):
+    if source not in SUPPORTED_DATA_SOURCES:
+        raise ValueError(f"Provided data source {source} is not supported.")
+
+    qa = RetrievalQA.from_chain_type(
+        llm = llm,
+        chain_type="stuff",
+        retriever = data_set.as_retriever(), # repla
+        verbose=True,
+    )
+
+    output = qa.invoke(query)
+
+    return output
 
 # determining prompts as per search selection option 
-if selection == "Wikipedia":
-    while not os.path.exists("./notifications/wiki_query.txt"):
-        time.sleep(5)
-    if os.path.exists("./notifications/wiki_query.txt"):
-        f1 = open("./notifications/wiki_query.txt", 'r')
-        query = f1.read()
-        f1.close()
-        print(f"wiki prompt after selection is {query}")
-elif selection == "Research Paper":
-    while not os.path.exists("./notifications/doc_query.txt"):
-        time.sleep(5)
-    if os.path.exists("./notifications/doc_query.txt"):
-        f2 = open("./notifications/doc_query.txt", 'r')
-        query = f2.read()
-        f2.close()
-        print(f"Docu prompt after selection is {query}")
-else:
-    while not os.path.exists("./notifications/cmd_qr2.txt.txt"):
-        time.sleep(5)
-    if os.path("./notifications/cmd_qr2.txt"):
-        f3 = open("./notifications/cmd_qr1.txt", 'r')
-        query1 = f3.read()
-        f4 = open("./notifications/cmd_qr2.txt", 'r')
-        query2 = f4.read()
-        f3.close()
-        f4.close()
-        print(f"wiki3 prompt after selection is {query1}")
-        print(f"docu3 prompt after selection is {query2}")
+def generate_answer(selection: DataSource):
+    if selection not in SUPPORTED_DATA_SOURCES:
+        raise ValueError(f"Provided data source {selection} is not supported.")
 
+    if selection == "Wikipedia":
+        while not os.path.exists("./notifications/wiki_query.txt"):
+            time.sleep(5)
 
+        query = get_query(selection)
+        data_set = load_data_set(selection, query)
+        response = retrieve_info("Wikipedia", data_set, query)
+    elif selection == "Research Paper":
+        while not os.path.exists("./notifications/doc_query.txt"):
+            time.sleep(5)
 
-# setting up info retreival from Wikipedia pages (1st knowledge source)
-if selection == "Wikipedia":
-    print(f"wiki prompt insider wrapper is {query}")
-    wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
-    wiki_output = wikipedia.run(query)
-
-
-
-# fragmegting the document content to fit in the number of token limitations
-if selection == "Wikipedia":
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 50)
-    new_docs = [Document(page_content=sent) for sent in wiki_output.split('\n')]
-
-# splitted_output = text_splitter.split_documents(new_doc)
-    data_set = DocArrayInMemorySearch.from_documents(new_docs, embedding=embeddings)
-
-
-
-# retreiving the llm response using user query
-if selection == "Wikipedia":
-    qa = RetrievalQA.from_chain_type(
-        llm =llm,
-        chain_type="stuff",
-        retriever = data_set.as_retriever(),
-        verbose=True,
-    )
-
-
-
-if selection.strip() == "Wikipedia":
-    wiki_out = qa.invoke(query)
-    with open("./notifications/wiki_out.txt", 'w') as fwk:
-        fwk.write(str(wiki_out)['result'])
-        pickle.dumps("wiki_out.txt")
-
-
-
-# Loading research paper from web source (2nd knoledge source)
-
-if selection == "Research Paper":
-    loader = PyPDFLoader("./2312.10997v5.pdf")
-    docs = loader.load()
-
-# fragmegting the document content to fit in the number of token limitations
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 50)
-    splits = text_splitter.split_documents(docs)
-
-# load the 
-    data_set = DocArrayInMemorySearch.from_documents(documents=splits, embedding=embeddings)
-
-
-
-# retreiving the llm response using user query
-
-if selection == "Research Paper":
-    qa = RetrievalQA.from_chain_type(
-        llm =llm,
-        chain_type="stuff",
-        retriever = data_set.as_retriever(),
-        verbose=True,
-    )
-
-
-
-# get query from U/I now
-if selection == "Research Paper":
-    result = qa.invoke(query)
-    with open("./notifications/doc_out.txt", 'w') as frp:
-        frp.write(str(result['result']))
-    pickle.dumps("doc_out.txt")
-
-
-
-# combining two RAG knoledge sources together for better performance
-
-if selection == "Both":
-    wiki_tool = Tool(
-        name = "wikipedia",
-        func = wikipedia.run,
-        description = "A useful tool to search internet for the requested information",
-    )
+        query = get_query(selection)
+        data_set = load_data_set(selection, query)
+        response = retrieve_info("Research Paper", data_set, query)
     
-    docsearch_tool = Tool(
-        name = "docsearch",
-        func = qa.run,
-        description = "A tool to search information from the pool of documents",
-    )
-    
-    agent= initialize_agent(
-        tools=tools,
-        llm=llm,
-        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-        verbose = False,
-        handle_pasring_errors = True,
-    )
-
-
-
-# executing the agent for both knowledgebase options
-
-if selection == "Both":
-    result1 = agent.invoke(str(query1))
-    result2 = agent.invoke(str(query2))
-    
-    with open("./notifications/1.txt", 'w') as f1:
-        f1.write(str(result1['output']))
-        pickle.dumps("1.txt")
-    with open("./notifications/2.txt", 'w') as f2:
-        f2.write(str(result2['output']))
-        pickle.dumps("2.txt")
-
-
-
-# exporting program ending indicator
-ffnl = open("./notifications/PROG EXIT.txt", "rb")
-pickle.dumps(123, ffnl)
-ffnl.close()
+    return response
--- rag_simplified_st.py
+++ rag_simplified_st.py
@@ -5,6 +5,8 @@ import os.path
 import time
 import pickle
 import json
+from RAG_Simplified import generate_answer
+
 # import warnings
 # warnings.filterwarnings("ignore")
 
@@ -31,7 +33,7 @@ del delete_files
 
 # function to execute backend python file
 def run_file():
-    call(["python", "RAG_Simplified_Modified.py"])
+    call(["python", "RAG_Simplified.py"])
 
 def wiki_choice() -> str:
     wiki_query = st.text_input(
@@ -58,14 +60,14 @@ def combined_choice():
     if st.button("Submit"):
         return combined_query1, combined_query2
 
-def routine_process():
-    with st.spinner("Processing your request..."):
-        # display = []
-        assert os.path.exists("C:/Users/Mast_Nijanand/RAG Simplified/notifications/wiki_out.json")
+# TODO: add spinner to main function instead of here
+def routine_process(answer: str):
+    # with st.spinner("Processing your request..."):
+    #     # display = []
+    #     assert os.path.exists("C:/Users/Mast_Nijanand/RAG Simplified/notifications/wiki_out.json")
     st.success("Data processing complete!")
-    with open("C:/Users/Mast_Nijanand/RAG Simplified/notifications/wiki_out.json", 'r') as fp:
-        output = json.load(fp)
-        st.markdown(f"### {output['result']}")
+    # with open("C:/Users/Mast_Nijanand/RAG Simplified/notifications/wiki_out.json", 'r') as fp:
+    st.markdown(f"### {answer['result']}")
 
 def dual_process(notify1, notify2):
     while st.spinner("Processing your request..."):
@@ -98,9 +100,8 @@ def dual_process(notify1, notify2):
             st.write(each)
     st.success("Data processing complete!")
 
-def main_func(option):
-#    run_file()
-    if option == "Wikipedia":
+def main(selection):
+    if selection == "Wikipedia":
         wiki_query = wiki_choice()
 
         if wiki_query is not None:
@@ -109,12 +110,9 @@ def main_func(option):
                 fp.write(str(wiki_query))
             pickle.dumps("wiki_query.txt")
 
-            run_file()
-            assert os.path.exists(check_notifications + "wiki_out.json")
-
-            routine_process()
-
-    elif (option == "Research Paper"):
+            answer = generate_answer(selection)
+            routine_process(answer)
+    elif selection == "Research Paper":
         research_query = research_choice()
 
         if research_query is not None:
@@ -123,49 +121,8 @@ def main_func(option):
                 fp.write(str(research_query))
             pickle.dumps("doc_query.txt")
 
-            run_file()
-
-            # loading run file output
-            while not os.path.exists(check_notifications + str("PROG EXIT")):
-                time.sleep(1)
-                if os.path.exists(check_notifications + str("doc_out.txt")):
-                    f2 = open('./notifications/doc_out.txt', 'r')
-                    doc_out = f2.read()
-                    f2.close()
-                    break
-            routine_process(notify = doc_out)
-
-    else:
-        cb_qr1, cb_qr2 = combined_choice()
-
-        if (cb_qr1 is not None) and (cb_qr2 is not None):
-            # storing both queries for further processing & exporting
-            fp = open("./notifications/cmd_qr1.txt", "w")
-            fp.write(str(cmd_qr1))
-            fp.close()
-            pickle.dumps("cmd_qr1.txt")
-
-            fq = open("./notifications/cmd_qr2.pkl", "w")
-            fq.write(str(cmd_qr2))
-            fq.close()
-            pickle.dumps("cmd_qr2.txt")
-
-            run_file()
-
-            # loading run file outputs
-            while not os.path.exists(check_notifications + str("PROG EXIT")):
-                time.sleep(1)
-                if os.path.exists(check_notifications + str("2.txt")):
-                    fr1 = open('./notifications/1.txt', 'r')
-                    one = fr1.read()
-                    fr1.close()
-
-                    fr2 = open('./notifications/2.txt', 'r')
-                    two = fr1.read()
-                    fr2.close()
-                    break
-            dual_process(notify1 = one, notify2 = two)
-
+            answer = generate_answer(selection)
+            routine_process(answer)
 
 if __name__ == "__main__":
     if Options:
@@ -177,21 +134,4 @@ if __name__ == "__main__":
     pickle.dumps("option.txt")
 
     # calling main function
-    main_func(st.session_state.selection)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    main(st.session_state.selection)
